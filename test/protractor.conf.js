a9dff97534ff07713dfdf52f720e06432cabe2f5
Benaka Moorthi <benaka.moorthi@mheducation.com>
2016-05-08 18:35:20 -0700

    Try to get protractor tests to run on saucelabs on travis.

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -10,3 +10,13 @@ exports.config = {
         slow: 3000
     }
 };
+
+if (process.env.TRAVIS) {
+    exports.config.sauceUser = process.env.SAUCE_USERNAME;
+    exports.config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+    exports.config.capabilities = {
+        'browserName': 'chrome',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER
+    };
+}
